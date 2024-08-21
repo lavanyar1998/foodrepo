6e944a48b5bfed757e292c495b8eb46d93ac9b63
lavanyar1998 <lavushimogga4@gmail.com>
2024-08-21 23:49:34 +0530

    first commit

---
+++ foodmodule/foodmodule/src/main/java/com/food/foodmodule/FoodmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.food.foodmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmoduleApplication.class, args);
+	}
+
+}
---
+++ foodmodule/foodmodule/src/test/java/com/food/foodmodule/FoodmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.foodmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
